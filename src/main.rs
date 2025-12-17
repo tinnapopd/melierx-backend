e8b2c0c18e578eec8472d9855d1f767b29af4f65
TeeTinnapop <tinnapop.d@securitypitch.com>
2025-12-17 23:44:04 +0700

    feat: recreate the project structure.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use melierx_backend::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let listener = std::net::TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
