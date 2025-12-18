460fae88474df4a34091b068609bc4f127553e95
TeeTinnapop <tinnapop.d@securitypitch.com>
2025-12-18 23:58:17 +0700

    feat: update to use the PgPool for connection and update test cases

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use melierx_backend::configuration::get_configuration;
 use melierx_backend::startup::run;
+use sqlx::PgPool;
+use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:8000")?;
-    run(listener)?.await
+    // Panic if we cannot read the configuration file
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,7 @@
 use actix_web::{HttpResponse, web};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -6,10 +9,26 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
-    println!(
-        "Received subscription: name={}, email={}",
-        form.name, form.email
-    );
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now().naive_utc()
+    )
+    .execute(pool.as_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            return HttpResponse::InternalServerError().finish();
+        }
+    };
+
     HttpResponse::Ok().finish()
 }
