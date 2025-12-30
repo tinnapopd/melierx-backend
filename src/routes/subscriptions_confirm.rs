c363f4747d1c1e304a306d51a3a921faeb6ef41d
TeeTinnapop <tinnapop.d@securitypitch.com>
2025-12-30 17:07:33 +0700

    style: add comment and sort implementation order in routes.

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -2,34 +2,33 @@ use actix_web::{HttpResponse, web};
 use sqlx::PgPool;
 use uuid::Uuid;
 
+// Query parameters structure for subscription confirmation.
 #[derive(serde::Deserialize)]
 pub struct Parameters {
     pub subscription_token: String,
 }
 
-/// Marks the subscriber as confirmed in the database.
+/// Handles the confirmation of a pending subscription.
 /// # Arguments
 /// * `pool` - A reference to the PostgreSQL connection pool.
-/// * `subscriber_id` - The UUID of the subscriber to be confirmed.
+/// * `parameters` - The query parameters containing the subscription token.
 /// # Returns
-/// A Result indicating success or failure of the operation.
-#[tracing::instrument(name = "Marking subscription as confirmed", skip(subscriber_id, pool))]
-pub async fn confirm_subscriber(pool: &PgPool, subscriber_id: Uuid) -> Result<(), sqlx::Error> {
-    sqlx::query!(
-        r#"
-        UPDATE subscriptions
-        SET status = 'confirmed'
-        WHERE id = $1
-        "#,
-        subscriber_id
-    )
-    .execute(pool)
-    .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-    })?;
-    Ok(())
+/// An HTTP response indicating the result of the confirmation process.
+#[tracing::instrument(name = "Confirm a pending subscription", skip(pool, parameters))]
+pub async fn confirm(pool: web::Data<PgPool>, parameters: web::Query<Parameters>) -> HttpResponse {
+    let id = match get_subscriber_id_from_token(&pool, &parameters.subscription_token).await {
+        Ok(id) => id,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
+    match id {
+        None => HttpResponse::Unauthorized().finish(),
+        Some(subscriber_id) => {
+            if confirm_subscriber(&pool, subscriber_id).await.is_err() {
+                return HttpResponse::InternalServerError().finish();
+            }
+            HttpResponse::Ok().finish()
+        }
+    }
 }
 
 /// Retrieves the subscriber ID associated with the given subscription token.
@@ -59,25 +58,27 @@ pub async fn get_subscriber_id_from_token(
     Ok(result.map(|r| r.subscriber_id))
 }
 
-/// Handles the confirmation of a pending subscription.
+/// Marks the subscriber as confirmed in the database.
 /// # Arguments
 /// * `pool` - A reference to the PostgreSQL connection pool.
-/// * `parameters` - The query parameters containing the subscription token.
+/// * `subscriber_id` - The UUID of the subscriber to be confirmed.
 /// # Returns
-/// An HTTP response indicating the result of the confirmation process.
-#[tracing::instrument(name = "Confirm a pending subscription", skip(pool, parameters))]
-pub async fn confirm(pool: web::Data<PgPool>, parameters: web::Query<Parameters>) -> HttpResponse {
-    let id = match get_subscriber_id_from_token(&pool, &parameters.subscription_token).await {
-        Ok(id) => id,
-        Err(_) => return HttpResponse::InternalServerError().finish(),
-    };
-    match id {
-        None => HttpResponse::Unauthorized().finish(),
-        Some(subscriber_id) => {
-            if confirm_subscriber(&pool, subscriber_id).await.is_err() {
-                return HttpResponse::InternalServerError().finish();
-            }
-            HttpResponse::Ok().finish()
-        }
-    }
+/// A Result indicating success or failure of the operation.
+#[tracing::instrument(name = "Marking subscription as confirmed", skip(subscriber_id, pool))]
+pub async fn confirm_subscriber(pool: &PgPool, subscriber_id: Uuid) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        UPDATE subscriptions
+        SET status = 'confirmed'
+        WHERE id = $1
+        "#,
+        subscriber_id
+    )
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
 }
