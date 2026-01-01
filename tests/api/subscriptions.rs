9bd5f4544548ea69938240f37be924c224b30836
TeeTinnapop <tinnapop.d@securitypitch.com>
2026-01-01 21:49:14 +0700

    test: update and format test cases.

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -7,7 +7,7 @@ use crate::helpers::spawn_app;
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app = spawn_app().await;
-    let body = "name=Tee%20Tinnapop&email=tinnapopduangtha%40gmail.com";
+    let body = "name=FirstName%20LastName&email=mynickname%40gmail.com";
 
     Mock::given(path("/email"))
         .and(method("POST"))
@@ -19,20 +19,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let response = app.post_subscriptions(body.into()).await;
 
     // Assert
-    assert_eq!(200, response.status().as_u16());
+    assert_eq!(response.status().as_u16(), 200);
 }
 
 #[actix_web::test]
 async fn subscribe_persists_the_new_subscriber_in_the_database() {
     // Arrange
     let app = spawn_app().await;
-    let body = "name=Tee%20Tinnapop&email=tinnapopduangtha%40gmail.com";
-
-    Mock::given(path("/email"))
-        .and(method("POST"))
-        .respond_with(ResponseTemplate::new(200))
-        .mount(&app.email_server)
-        .await;
+    let body = "name=FirstName%20LastName&email=mynickname%40gmail.com";
 
     // Act
     app.post_subscriptions(body.into()).await;
@@ -43,67 +37,34 @@ async fn subscribe_persists_the_new_subscriber_in_the_database() {
         .await
         .expect("Failed to fetch saved subscription.");
 
-    assert_eq!(saved.email, "tinnapopduangtha@gmail.com");
-    assert_eq!(saved.name, "Tee Tinnapop");
+    assert_eq!(saved.email, "mynickname@gmail.com");
+    assert_eq!(saved.name, "FirstName LastName");
     assert_eq!(saved.status, "pending_confirmation");
 }
 
 #[actix_web::test]
-async fn subscribe_returns_a_400_when_data_is_missing() {
+async fn subscribe_fails_if_there_is_a_fatal_database_error() {
     // Arrange
     let app = spawn_app().await;
-    let test_cases = vec![
-        ("name=Tee%20Tinnapop", "missing the email"),
-        ("email=tinnapopduangtha%40gmail.com", "missing the name"),
-        ("", "missing both name and email"),
-    ];
+    let body = "name=FirstName%20LastName&email=mynickname%40gmail.com";
 
-    for (invalid_body, error_message) in test_cases {
-        // Act
-        let response = app.post_subscriptions(invalid_body.into()).await;
+    sqlx::query!("ALTER TABLE subscriptions DROP COLUMN email;",)
+        .execute(&app.db_pool)
+        .await
+        .unwrap();
 
-        // Assert
-        assert_eq!(
-            400,
-            response.status().as_u16(),
-            "The API did not fail with 400 Bad Request when the payload was {}.",
-            error_message
-        );
-    }
-}
+    // Act
+    let response = app.post_subscriptions(body.into()).await;
 
-#[actix_web::test]
-async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
-    // Arrange
-    let app = spawn_app().await;
-    let test_cases = vec![
-        ("name=&email=tinnapopduangtha%40gmail.com", "empty name"),
-        ("name=Tee%20Tinnapop&email=", "empty email"),
-        (
-            "name=Tee%20Tinnapop&email=definitely-not-an-email",
-            "invalid email",
-        ),
-    ];
-
-    for (body, description) in test_cases {
-        // Act
-        let response = app.post_subscriptions(body.into()).await;
-
-        // Assert
-        assert_eq!(
-            400,
-            response.status().as_u16(),
-            "The API did not return a 400 Bad Request when the payload was {}.",
-            description
-        );
-    }
+    // Assert
+    assert_eq!(response.status().as_u16(), 500);
 }
 
 #[actix_web::test]
 async fn subscribe_sends_a_confirmation_email_for_valid_data() {
     // Arrange
     let app = spawn_app().await;
-    let body = "name=Tee%20Tinnapop&email=tinnapopduangtha%40gmail.com";
+    let body = "name=FirstName%20LastName&email=mynickname%40gmail.com";
 
     Mock::given(path("/email"))
         .and(method("POST"))
@@ -122,7 +83,7 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
 async fn subscribe_sends_a_confirmation_email_with_a_link() {
     // Arrange
     let app = spawn_app().await;
-    let body = "name=Tee%20Tinnapop&email=tinnapopduangtha%40gmail.com";
+    let body = "name=FirstName%20LastName&email=mynickname%40gmail.com";
 
     Mock::given(path("/email"))
         .and(method("POST"))
@@ -142,19 +103,52 @@ async fn subscribe_sends_a_confirmation_email_with_a_link() {
 }
 
 #[actix_web::test]
-async fn subscribe_fails_if_there_is_a_fatal_database_error() {
+async fn subscribe_returns_a_400_when_data_is_missing() {
     // Arrange
     let app = spawn_app().await;
-    let body = "name=Tee%20Tinnapop&email=tinnapopduangtha%40gmail.com";
+    let test_cases = vec![
+        ("name=FirstName%20LastName", "missing the email"),
+        ("email=mynickname%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
 
-    sqlx::query!("ALTER TABLE subscription_tokens DROP COLUMN subscription_token;")
-        .execute(&app.db_pool)
-        .await
-        .unwrap();
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = app.post_subscriptions(invalid_body.into()).await;
 
-    // Act
-    let response = app.post_subscriptions(body.into()).await;
+        // Assert
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
 
-    // Assert
-    assert_eq!(500, response.status().as_u16());
+#[actix_web::test]
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
+    // Arrange
+    let app = spawn_app().await;
+    let test_cases = vec![
+        ("name=&email=mynickname%40gmail.com", "empty name"),
+        ("name=FirstName%20LastName&email=", "empty email"),
+        (
+            "name=FirstName%20LastName&email=definitely-not-an-email",
+            "invalid email",
+        ),
+    ];
+
+    for (body, description) in test_cases {
+        // Act
+        let response = app.post_subscriptions(body.into()).await;
+
+        // Assert
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "The API did not return a 400 Bad Request when the payload was {}.",
+            description
+        );
+    }
 }
