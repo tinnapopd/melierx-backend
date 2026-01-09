106fa216c5b7eea3806d76b666516423adceec7e
TeeTinnapop <tinnapop.d@securitypitch.com>
2026-01-09 07:46:38 +0700

    style: remove unneccesary comments.

--- tests/api/admin_dashboard.rs
+++ tests/api/admin_dashboard.rs
@@ -2,53 +2,41 @@ use crate::helpers::{assert_is_redirect_to, spawn_app};
 
 #[actix_web::test]
 async fn you_must_be_logged_in_to_access_the_admin_dashboard() {
-    // Arrange
     let app = spawn_app().await;
-
-    // Act
     let response = app.get_admin_dashboard().await;
-
-    // Assert
     assert_is_redirect_to(&response, "/login");
 }
 
 #[actix_web::test]
 async fn logout_clears_session_state() {
-    // Arrange
     let app = spawn_app().await;
 
-    // Act - Login
+    // Part1 - Login
     let login_body = serde_json::json!({
         "username": &app.test_user.username,
         "password": &app.test_user.password,
     });
     let response = app.post_login(&login_body).await;
-
-    // Assert - Logged in
     assert_is_redirect_to(&response, "/admin/dashboard");
 
-    // Act - Follow the redirect
+    // Part2 - Follow the redirect
     let html_page = app.get_admin_dashboard_html().await;
-
-    // Assert - Admin dashboard content
     assert!(
         html_page.contains(&format!("Welcome {}!", &app.test_user.username))
     );
 
-    // Act - Logout
+    // Part3 - Logout
     let response = app.post_logout().await;
     assert_is_redirect_to(&response, "/login");
 
-    // Act - Follow the redirect
+    // Part4 - Follow the redirect
     let html_page = app.get_login_html().await;
     assert!(
         html_page
             .contains(r#"<p><i>You have successfully logged out.</i></p>"#)
     );
 
-    // Act - Try to access admin dashboard again
+    // Part5 - Try to access admin dashboard again
     let response = app.get_admin_dashboard().await;
-
-    // Assert - Redirected to login page
     assert_is_redirect_to(&response, "/login");
 }
